35555d63d420dc063274c561bc8406ac8c94861e
deepakjaisinghani <103099147+deepakjaisinghani@users.noreply.github.com>
2025-03-04 14:25:01 +0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get('/api/data', (req, res) => {
+  const filePath = path.join(__dirname, 'data.json');
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).json({ error: 'Failed to read JSON file' });
+    } else {
+      res.setHeader('Content-Type', 'application/json');
+      res.send(data);
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
